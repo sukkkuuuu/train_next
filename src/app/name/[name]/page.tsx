1a27c5115e1ec4c166f1a75d3911c928743bf778
sukkkuuuu <sunavy900@gmail.com>
2025-10-01 05:34:55 +0900

    import Link 추가

--- src/app/name/[name]/page.tsx
+++ src/app/name/[name]/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Name({ params }: { params: { name: string } }) {
   return (
     <main>
       <h1 className="title">Name</h1>
       <p className="msg">당신은 `{params.name}`님이군요</p>
       <div>
-        <a href="/">go back!</a>
+        <Link href="/">go back!</Link>
       </div>
     </main>
   );
--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Name({ params }: { params: { name: string } }) {
   return (
     <main>
       <h1 className="title">Name page</h1>
       <p className="msg">/name/OO 와 같이 이름을 붙여서 접속하세요.</p>
       <div>
-        <a href="/">go back!</a>
+        <Link href="/">go back!</Link>
       </div>
     </main>
   );
