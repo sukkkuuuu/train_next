37c4dcc8c3356272b771b3e0fe9a723648014f1a
sukkkuuuu <sunavy900@gmail.com>
2025-10-01 05:28:57 +0900

    보기 싫은 오류 및 a->Link 컴포넌트로 변경

--- src/app/name/[name]/[age]/page.tsx
+++ src/app/name/[name]/[age]/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Name({
   params,
 }: {
@@ -7,10 +9,10 @@ export default function Name({
     <main>
       <h1 className="title">Name</h1>
       <p className="msg">
-        당신은 '{params.name}'님이군요 '{params.age}'살입니다.
+        당신은 {params.name}님이군요 {params.age}살입니다.
       </p>
       <div>
-        <a href="/">go back!</a>
+        <Link href="/">go back!</Link>
       </div>
     </main>
   );
