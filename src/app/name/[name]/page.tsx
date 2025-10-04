4c149b2b0157636d81d37efcff9ea2f255fb4441
sukkkuuuu <sunavy900@gmail.com>
2025-10-04 18:14:47 +0900

    app router의 동적,정적 렌더링 사용

--- src/app/name/[name]/page.tsx
+++ src/app/name/[name]/page.tsx
@@ -1,12 +1,33 @@
+"use server";
+
 import Link from "next/link";
 
-export default function Name({ params }: { params: { name: string } }) {
+const paths = [{ name: "kim" }, { name: "lee" }, { name: "park" }];
+
+export async function generateStaticParams() {
+  return paths;
+}
+
+export default async function Name({ params }) {
+  const result = paths.some((path) => path.name === params.name);
+
   return (
     <main>
-      <h1 className="title">Name</h1>
-      <p className="msg">당신은 `{params.name}`님이군요</p>
+      {result ? (
+        <>
+          <h1 className="title">Name = &apos;{params.name}&apos;</h1>
+          <p className="msg">{params.name}님 안녕하세요 !</p>
+        </>
+      ) : (
+        <>
+          <h1 className="title">&apos;{params.name}&apos;</h1>
+          <p className="msg">
+            &apos;{params.name}&apos;은(는) 사용할 수 없습니다.
+          </p>
+        </>
+      )}
       <div>
-        <Link href="/">go back!</Link>
+        <Link href="/">go Back!!</Link>
       </div>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,26 @@
 "use client";
-import Link from "next/link";
-// import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function Home() {
-  // const [input, setInput] = useState("");
-  // const [message, setMessage] = useState("your name");
-  // const doChange = (event) => {
-  //   setInput(event.target.value);
-  // };
-  // const doClick = () => {
-  //   setMessage("Hello" + input + "!!");
-  //   setInput("");
-  // };
+function SearchParamsContent() {
+  const searchParams = useSearchParams();
   return (
     <main>
-      <h1 className="title">Next.js sample.</h1>
-      <p className="msg">This is other page sample</p>
-      {/* <p className="msg">{message}</p> */}
-      <div>
-        <Link href="/other"> go other page</Link>
-      </div>
-      {/* <div className="m-5">
-        <input
-          type="text"
-          onChange={doChange}
-          value={input}
-          className="input"
-        />
-        <button onClick={doClick} className="btn">
-          Click
-        </button>
-      </div> */}
+      <h1 className="title">Index page</h1>
+      <ul>
+        {" "}
+        # 파라미터
+        <li>ID : {searchParams.get("id")}</li>
+        <li>PASS: {searchParams.get("pass")}</li>
+      </ul>
     </main>
+  );
+}
+
+export default function Home() {
+  return (
+    <Suspense>
+      <SearchParamsContent />
+    </Suspense>
   );
 }
